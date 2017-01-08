d5dfa0d010b14503305b78f5bdae9eac5f9d858c
samuraikun <tazitaziawawa@gmail.com>
2017-01-08 13:49:43 +0900

    finish setup

---
+++ README.md
@@ -0,0 +1,10 @@
+= Baukis_v5  - Rails5で開発した顧客管理システム
+
+== 説明
+
+書籍「実践Ruby on Rails」を元に作成した Baukis は企業向けの顧客管理システム（Ruby on Rails 学習用サンプル）です。
+
+== 推奨されるシステム環境
+
+* Ruby 2.3.1
+* MySQL 5.6 またはPostgreSQL 9.3
---
+++ config/application.rb
@@ -0,0 +1,28 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Baukis
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    config.time_zone = 'Tokyo'
+    config.i18n.load_path +=
+      Dir[Rails.root.join('config', 'locales', '**', '*{rb.yml}').to_s]
+    config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.helper false
+      g.assets false
+      g.test_framework :rspec
+      g.controller_specs false
+      g.view_specs false
+    end
+  end
+end
